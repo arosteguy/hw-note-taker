3e8c2f7924aeb87f939589b3be405533e7016130
cake <larosteguy@gmail.com>
2020-01-26 21:28:34 -0700

    work on wiring up

--- apiRoutes.js
+++ apiRoutes.js
@@ -6,3 +6,10 @@ app.get("/api/notes", function(req, res) {
     res.json(noteData);
   });
 };
+app.post.post("./api/notes", function (req, res) {
+    noteData.push(req.body);
+    res.json(true);
+});
+app.post("./api/clear", function() {
+    noteData = [];
+})
